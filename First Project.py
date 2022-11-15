a91c52f0961be626287aa3ffdd0e19b6f9a923ae
Kevin Barath <kevinbarath4@gmail.com>
2022-11-15 14:04:20 -0500

    Better Cool Calculator
    Universal Calculator so that now you can input an operator sign and give you that desired number between two variables. No added arithmetic operators from the last calculator

--- First Project.py
+++ First Project.py
@@ -1,23 +1,22 @@
-# Basic Aritmetic calculaor for two numbers WIP
 
-def Addition(num1, num2):
-    return int (num1) + int (num2)
-def Subtraction(num1, num2):
-    return int (num1) - int (num2)
-def Multiplication(num1, num2):
-    return int (num1) * int (num2)
-def Divison(num1, num2):
-    return float (num1) / float (num2)
-def Mod(num1, num2):
-    return float (num1) % float (num2)
+unum1 = float(input("Please Enter a First Number: "))
+uope1 = input("Please Enter an Operator (+ - / * %): ")
+unum2 = float(input("Please Enter a Second Number: "))
 
-unum1 = input("Please Enter a Number: ")
-unum2 = input("Please Enter a Number: ")
+if uope1 == "+":
+    print(float (unum1) + float (unum2))
 
-Addition = Addition(unum1, unum2)
-Subtraction = Subtraction(unum1, unum2)
-Multiplication = Multiplication(unum1, unum2)
-Division = Divison(unum1, unum2)
-Mod = Mod(unum1, unum2)
+elif uope1 == "-":
+    print(float (unum1) - float (unum2))
 
-print (str(Addition) + "\n" + str(Subtraction) + "\n" + str(Multiplication) + "\n" + str(Division) + "\n" + str(Mod))
+elif uope1 == "/":
+    print(float (unum1) / float (unum2))
+
+elif uope1 == "%":
+     print(float (unum1) % float (unum2))
+
+elif uope1 == "*":
+    print(float (unum1) * float (unum2))
+
+else:
+    print ("Please Restart Program, Issue with entered variables or operator")
