abeb6e9a8bf18911411cf50bf41475a54e80d1c8
Kevin Barath <kevinbarath4@gmail.com>
2022-11-16 01:33:36 -0500

    Word Translator
    A basic word translator that uses for loops and nested if statements to change string to my desired want. Essentially, every vowel in the typed phrase will be converted to the letter "K" or "k" depending on how it was originally processed

---
+++ Word Translator.py
@@ -0,0 +1,15 @@
+# changes every vowel (AEIOU) to the letter "K"
+def translate(word):
+    ftranslate = ""
+    for letter in word:
+        if letter.lower in "aeiou":
+            if letter.isupper():
+                tfranslate = ftranslate + "K"
+            else:
+                ftranslate = ftranslate + "k"
+        else:
+            ftranslate = ftranslate + letter
+
+    return ftranslate
+
+print(translate(input("Enter a phrase for translation: ")))
